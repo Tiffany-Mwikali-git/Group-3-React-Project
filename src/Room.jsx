49290e8d166ebd75994cb6442531a3d176b61791
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-07-24 15:31:41 +0300

    Adds functionroom and export.

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,3 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import '../index.css';
+
+function Room() {
+    return (
+        <div>
+            {/* Room content goes here */}
+        </div>
+    )
+};
+
+export default Room;
