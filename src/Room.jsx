ea6e0a2c4de3a40ea7f0e756dd4bdc776a62c2f8
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-07-24 15:56:50 +0300

    Adds use state variables.

--- src/Room.jsx
+++ src/Room.jsx
@@ -3,6 +3,29 @@ import { useNavigate, useParams } from 'react-router-dom';
 import '../index.css';
 
 function Room() {
+    	
+Alex Rooney Mwangi
+3:13 PM
+
+
+
+
+
+
+
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import '../index.css';
+
+function Room() {
+
+  const {id:roomId} = useParams();
+  console.log(roomId)
+  const navigate = useNavigate();
+  const [answer, setAnswer] = useState('');
+  const [showHint, setShowHint] = useState(false);
+  const [puzzle, setPuzzle] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
     return (
         <div>
             {/* Room content goes here */}
