fbfa701192237b5bff234cd6afbe1afafaa92936
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-07-25 10:30:26 +0300

    fixed styles .css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Intro from './components/Intro';
 import Room from './components/Room';
 import Victory from './components/Victory';
+import "./components/styles .css"
 
 function App() {
   return (
